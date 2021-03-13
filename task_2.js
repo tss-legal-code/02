7156bb824ee710d106b1c3bdb76834e575464454
tss-legal-code <s.tarabanchuk@gmail.com>
2021-03-13 15:14:58 +0200

    parts 1, 2 & 3 of the  hometask 2 are ready for tests

--- task_2.js
+++ task_2.js
@@ -1,7 +1,7 @@
 function makeObjectDeepCopy(objectToCopy) {
-    if (typeof(objectToCopy) != "object" && objectToCopy != null) {
+    if (typeof(objectToCopy) != "object" || objectToCopy == null) {
         return undefined;
-    }
+    };
     if (typeof objectDeepCopy == "undefined") {
         var objectDeepCopy = {};
     };
@@ -9,61 +9,57 @@ function makeObjectDeepCopy(objectToCopy) {
         if (typeof(objectToCopy[key]) == "object" && objectToCopy[key] != null) {
             if (Array.isArray(objectToCopy[key])) {
                 objectDeepCopy[key] = Object.values(makeObjectDeepCopy(objectToCopy[key]))
-            } else {
-                objectDeepCopy[key] = makeObjectDeepCopy(objectToCopy[key]);
             }
-        } else {
-            objectDeepCopy[key] = objectToCopy[key];
+            objectDeepCopy[key] = makeObjectDeepCopy(objectToCopy[key]);
         }
+        objectDeepCopy[key] = objectToCopy[key];
     }
     return objectDeepCopy
 }
-// + 2) Написать функцию, которая принимает три аргумента: массив чисел, первое значение интервала, второе значение интервала.
-// + Результатом вызова функции должен быть массив из значений, переданного массива, которые входят в этот интервал (концы включая).
-// + Договоримся использовать только массивы чисел.
-// + При этом значения интервала могут быть только числами.
-// + Если в качестве первого параметра передан не массив или массив, содержащий не только числа - кидать ошибку (throw new Error('error message')).
-// + Если хотя бы одно из значений интервала - невалидное число - также кидать ошибку.
-// + Если первое значение интервала < второго, то считать за интервал значения от первого до второго, в ином случае - от второго до первого.
-// Функция должна называться selectFromInterval. Это важно, т.к. проверять буду тестами.
-// > Примеры:
-// Вызываем функцию: selectFromInterval([1,3,5], 5, 2)
-// Получаем результат: [3,5]
-// Вызываем функцию: selectFromInterval([-2, -15, 0, 4], -13, -5)
-// Получаем результат: []
-// Вызываем функцию: selectFromInterval(['aaa'], 2, 3)
-// Получаем результат: Ошибка!
-
-
 
 function selectFromInterval(arrayOfNumbers, first, second) {
-
     if ((Array.isArray(arrayOfNumbers) && !arrayOfNumbers.some(isNaN)) &&
         !isNaN(first) && Number.isInteger(+first) &&
         !isNaN(second) && Number.isInteger(+second)) {
         let [begin, end] = (first < second) ? [first, second] : [second, first];
-        // условие и примеры прямо не требуют "реверсивного среза" массива
-        // при отрицательных значениях интервалов
         if (begin > arrayOfNumbers.length ||
             end < 1) {
-            return []
+            return [];
         }
         begin = (begin < 1) ? 1 : begin;
         end = (end > arrayOfNumbers.length) ? arrayOfNumbers.length : end;
-        return arrayOfNumbers.slice(--begin, end)
-    } else {
-        throw new Error('Ошибка!');
+        return arrayOfNumbers.slice(--begin, end);
     }
+    throw new Error('Ошибка!');
 }
-console.log(selectFromInterval([1, 3, 5], -2, 5));
-console.log(selectFromInterval([1, 3, 5], 2, -5));
-console.log(selectFromInterval([1, 3, 5], -2, -5));
 
+const myIterable = {
+    from: 1,
+    to: 4,
+    [Symbol.iterator]: function() {
+        if (typeof(this.from) == "number" &&
+            typeof(this.to) == "number" &&
+            this.from < this.to) {
+            return {
+                current: this.from,
+                last: this.to,
+                next() {
+                    if (this.current <= this.last) {
+                        return {
+                            value: this.current++,
+                            done: false,
+                        };
+                    };
+                    return {
+                        done: true,
+                    };
+                },
+            };
+        };
+        throw new Error('Ошибка!');
+    },
+}
 
-
-// console.log(selectFromInterval([1, 3, 5], 5, 2));
-// // [3,5]
-// console.log(selectFromInterval([-2, -15, 0, 4], -13, -5));
-// // []
-// console.log(selectFromInterval(['aaa'], 2, 3))
-// Ошибка
+for (let num of myIterable) {
+    console.log(num);
+}
